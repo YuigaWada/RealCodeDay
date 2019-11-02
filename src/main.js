c96ca92ca6570eb22d9d21c03f57f878060e14f6
YuigaWada <yuigawada@gmail.com>
2019-11-02 16:48:14 +0900

    Update to 1.0

--- src/main.js
+++ src/main.js
@@ -1,22 +1,43 @@
-const session = sessionStorage.getItem('visitedCodeDayUrl')
+// MIT License
+//
+// Copyright (c) 2019 YuigaWada
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
+
+
+const session = sessionStorage.getItem('visitedCodeDayUrl')
 const selector = '#submitForm > div > div.col-xs-12.col-md-4.side-right > div:nth-child(4) > div.panel-body > span > a'
 const editPageTemplate = 'https://publish.codeday.me/post/publist?site=jp&id='
 
-
-window.onload = function() { // ** MAIN ** //
+$(document).ready( function(){ // ** MAIN ** //
   console.log('FuckCodeDay is loaded correctly.')
   var shapedUrl = removeQuery(window.location)
 
   var hasVisited = session == shapedUrl
-
   if (hasVisited) {
     sessionStorage.removeItem('visitedCodeDayUrl')
     return
   } else {
     sessionStorage.setItem('visitedCodeDayUrl', shapedUrl)
+    displayLoading()
   }
-
 
 
 
@@ -24,9 +45,9 @@ window.onload = function() { // ** MAIN ** //
   if (!isEditingPage) { // Assumes that this page is like https://codeday.me/jp/qa/〜
     var postId = getPostId(shapedUrl)
 
-    var ediPageUrl = editPageTemplate + postId
+    var editPageUrl = editPageTemplate + postId
     $.ajax({
-        url: ediPageUrl
+        url: editPageUrl
       })
       .then(
         //success
@@ -41,8 +62,13 @@ window.onload = function() { // ** MAIN ** //
   } else {
     jump2Target(document.querySelector(selector))
   }
-}
+})
 
+function displayLoading() {
+  document.getElementsByTagName('html')[0].innerHTML
+  = "<div align=\"center\"><h1>Redirecting...</h1>" +
+    "<p>FuckCodeDay by <a href=\"https://twitter.com/yuigawda\">Yuiga Wada</a></p></div>"
+}
 
 function removeQuery(urlInfo) {
   var newUrl = urlInfo.origin + urlInfo.pathname
